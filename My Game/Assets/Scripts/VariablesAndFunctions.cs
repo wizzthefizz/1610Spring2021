6729550f7d097a5b8ed8954275802c3153e47534
Emmorie Decker <decker.emmorie@gmail.com>
2021-01-27 09:34:34 -0700

    Variables and functions

---
+++ My Game/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 5;
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
