06f71ebfa5461ed6fabf31c883a0997aebb03a37
Emmorie Decker <decker.emmorie@gmail.com>
2021-01-29 11:12:25 -0700

    If Statements

---
+++ My Game/Assets/Scripts/BeginnerScripting/IfStatements.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    private float coffeeTemperature = 85.0f;
+
+    private float hotLimitTemperature = 70.0f;
+
+    private float coldLimitTemperature = 40.0f;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TemperatureTest();
+            coffeeTemperature -= Time.deltaTime * 5f;
+        }
+    }
+
+    void TemperatureTest()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot");
+        }
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold");
+        }
+        else
+        {
+            print("Coffee is just right");
+        }
+    }
+}
---
+++ My Game/Assets/Scripts/BeginnerScripting/MainPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    public string MyName;
+
+    private void Start()
+    {
+        Debug.Log("I am alive and my name is " + MyName);
+    }
+}
