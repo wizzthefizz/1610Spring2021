5c320716f39a24a67099932660444137a274800e
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-01 22:24:05 -0700

    ForEachLoop

--- My Game/Assets/Scripts/BeginnerScripting/ForEachLoop.cs
+++ My Game/Assets/Scripts/BeginnerScripting/ForEachLoop.cs
@@ -7,11 +7,12 @@ public class ForEachLoop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string[] strings = new string[3];
+        string[] strings = new string[4];
 
         strings[0] = "First string";
         strings[1] = "Second string";
         strings[2] = "Third string";
+        strings[3] = "Fourth string";
 
         foreach (string item in strings)
         {
