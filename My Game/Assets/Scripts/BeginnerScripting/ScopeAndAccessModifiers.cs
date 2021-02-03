e847be31a05f16bcd312eeec59b184d7bd7e9382
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-02 20:07:15 -0700

    Scope and access modifiers

---
+++ My Game/Assets/Scripts/BeginnerScripting/ScopeAndAccessModifiers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+    public int alpha = 5;
+
+    private int beta = 0;
+    private int gamma = 5;
+
+    private AnotherClass myOtherClass;
+    void Start()
+    {
+        alpha = 29;
+        
+        myOtherClass = new AnotherClass();
+        myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+    }
+
+    void Example(int pens, int crayons)
+    {
+        int answer;
+        answer = pens * crayons * alpha;
+        Debug.Log(answer);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Alpha is set to " + alpha);
+    }
+}
