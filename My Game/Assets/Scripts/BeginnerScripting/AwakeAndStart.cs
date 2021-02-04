091d5cfc2c61ce708893e6bc84bd934a969a1a0d
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-03 21:46:21 -0700

    Awake and Start

---
+++ My Game/Assets/Scripts/BeginnerScripting/AwakeAndStart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+   
+}
---
+++ My Game/Assets/Scripts/BeginnerScripting/UpdateAndFixedUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time: " + Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Update time: " + Time.deltaTime);
+    }
+}
