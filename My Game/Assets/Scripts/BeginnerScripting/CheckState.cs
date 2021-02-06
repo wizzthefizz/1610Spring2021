97d6dbb9166bf2f5ef8ffa37e37b9986e5404c83
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-05 22:52:19 -0700

    Activating Game Objects

---
+++ My Game/Assets/Scripts/BeginnerScripting/CheckState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Active self: " + myObject.activeSelf);
+        Debug.Log("Active in hierarchy" + myObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
