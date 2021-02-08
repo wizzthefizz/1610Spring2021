7cec1197dde1c23a5bde90d157de7e4391e8c9c5
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-08 10:36:14 -0700

    added Counter Script

---
+++ Truck Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public int numberCount;
+    public Text CounterText;
+    
+    
+    void Update()
+    {
+        numberCount++;
+        CounterText.text = numberCount.ToString();
+    }
+}
