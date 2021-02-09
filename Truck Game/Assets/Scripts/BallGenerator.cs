bd7496a714a8d9c7b0983c5d09ac4f91a275dc86
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-09 10:26:38 -0700

    Ball Generator

---
+++ Truck Game/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+    private int number = 0;
+
+    private IEnumerator Start()
+    {
+        while (number < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+            number++;
+        }
+    }
+}
