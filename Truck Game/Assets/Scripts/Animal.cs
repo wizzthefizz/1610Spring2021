e7e602977194b62f2454e63a84752148cab7e7da
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-10 09:50:45 -0700

    Added Animal and mammal scripts

---
+++ Truck Game/Assets/Scripts/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+   public Color animalColor = Color.red;
+   public float speed = 1f;
+   public string animalName = "Bob";
+   public int health = 10;
+
+   private void Start()
+   {
+      Debug.Log("Hello world!");
+   }
+}
