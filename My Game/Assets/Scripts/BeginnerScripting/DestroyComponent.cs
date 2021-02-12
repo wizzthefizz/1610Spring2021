472eee12461ec1dae6559e49cb23a7638b8ba02b
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-11 23:01:54 -0700

    Destroy Scripts

---
+++ My Game/Assets/Scripts/BeginnerScripting/DestroyComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyComponent : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
---
+++ My Game/Assets/Scripts/BeginnerScripting/DestroyOther.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
