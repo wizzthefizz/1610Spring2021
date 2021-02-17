d4d5caf2dda24698779ebece13f838288a0e66cd
Emmorie Decker <decker.emmorie@gmail.com>
2021-02-16 22:18:38 -0700

    DataType

---
+++ My Game/Assets/Scripts/BeginnerScripting/DataType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataType : MonoBehaviour
+{
+    void Start () 
+    {
+        //Value type variable
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+        
+        //Reference type variable
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
+    }
+}
