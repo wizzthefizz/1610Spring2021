a383db2a0cba4c63a5c0981cf198aab35718f4a1
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-04 21:29:43 -0700

    Get Raw Axis

---
+++ My Game/Assets/Scripts/BeginnerScripting/AxisRawExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisRawExample : MonoBehaviour
+{
+    public float range;
+    public GUIText textOutput;
+    
+    
+    void Update () 
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+        
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: "+h.ToString("F2");  
+    }
+}
