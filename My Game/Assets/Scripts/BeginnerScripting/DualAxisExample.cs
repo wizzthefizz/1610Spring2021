01c3efe6db78f1bf21a388674466ac5dc0adb3c2
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-07 21:43:01 -0700

    Dual Axis

---
+++ My Game/Assets/Scripts/BeginnerScripting/DualAxisExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using 
+
+public class DualAxisExample : MonoBehaviour
+{
+    public float range;
+    public GUIText textOutput;
+    
+    
+    void Update () 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * range;
+        float yPos = v * range;
+        
+        transform.position = new Vector3(xPos, yPos, 0);
+        textOutput.text = Convert.ToBoolean("Horizontal Value Returned: " + h.ToString("F2") +
+                                            "\nVertical Value Returned: " + v.ToString("F2"));    
+    }
+}
