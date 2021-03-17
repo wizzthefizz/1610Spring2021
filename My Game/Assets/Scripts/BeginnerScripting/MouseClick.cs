5cd119d77f40272e06504af382e0641f254e083e
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-16 20:19:43 -0600

    Beginner scripting Mouse Click

---
+++ My Game/Assets/Scripts/BeginnerScripting/MouseClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    void OnMouseDown ()
+    {
+        rigidbody.AddForce(-transform.forward * 500f);
+        rigidbody.useGravity = true;
+    }
+}
