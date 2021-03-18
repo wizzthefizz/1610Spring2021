7ebf1f8d91542a0441e1240aabb45ea1bcd3f004
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-17 19:28:34 -0600

    Trigger 2D Events

---
+++ Truck Game/Assets/Scripts/Trigger2DBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Trigger2DBehavior : MonoBehaviour
+{
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter2D(Collider2D other)
+    {   
+        triggerEvent.Invoke();
+    }
+}
