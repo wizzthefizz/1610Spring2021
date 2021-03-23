c3bd2caaa1aa230733ce8838a0c46b387efbf70b
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-22 20:48:55 -0600

    properties

---
+++ My Game/Assets/Scripts/BeginnerScripting/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    void Start () 
+    {
+        Player myPlayer = new Player();
+
+        //Properties can be used just like variables
+        myPlayer.Experience = 5;
+        int x = myPlayer.Experience;
+    }
+}
