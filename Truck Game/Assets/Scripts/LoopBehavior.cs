a2f88dac55ff9186863bf5c63bc01af1ab7cd721
Emmorie Decker <decker.emmorie@gmail.com>
2021-03-30 19:20:46 -0600

    Weapons and Foreach loops

---
+++ Truck Game/Assets/Scripts/LoopBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehavior : MonoBehaviour
+{
+    public int number, total;
+
+    public string[] cars;
+
+    public Weapon[] weapons;
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            total = i;
+            print(total);
+        }
+
+        foreach (var car in cars)
+        {
+            print("I have a "+ car);
+        }
+
+        foreach (var weapon in weapons)
+        {
+            print("You have my "+ weapon.name + "with a value of" + weapon.powerLevel);
+        }
+    }
+    
+}
